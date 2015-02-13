4b2ac0c701d16024fe9e26030bca20b2fa7a743d
Ilya Rubinshteyn <ilya_rubinshteyn@Ilya-2.local>
2015-02-12 19:55:30 -0800

    Adding selenium java projects

---
+++ adv_sel/Selenium_Class_1/src/Day1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.regex.*;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.*;
+import org.openqa.selenium.chrome.*;
+
+public class Day1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
